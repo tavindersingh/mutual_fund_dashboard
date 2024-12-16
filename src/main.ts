01e8f74ab6815a667b31ef7784ff75c3c1bd170f
Tavinder Singh <tavinder123singh@gmail.com>
2024-12-16 18:47:54 +0530

    added swagger support

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Mutual Funds Apis')
+    .setDescription('Mutual Funds Apis description')
+    .setVersion('0.1')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
